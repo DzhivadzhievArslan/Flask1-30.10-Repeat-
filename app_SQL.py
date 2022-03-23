9dfcd3e2792dc532de529aa808bde90dfbdbb95d
Arslan <arsymaster@gmail.com>
2022-03-23 19:26:25 +0300

    Добавлены методы GET, POST с использованием ORM SQLAlchemy

--- app_SQL.py
+++ app_SQL.py
@@ -1,15 +1,9 @@
 from flask import Flask, jsonify, abort, request
 from random import choice
 from tools_db import create_connection, execute_query, execute_read_query, BASE_DIR
-from flask_sqlalchemy import SQLAlchemy
-from pathlib import Path
-
-BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'test.db'}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 quotes = [
     {
@@ -35,44 +29,30 @@ quotes = [
 
 ]
 
-db = SQLAlchemy(app)
-
-
-class QuoteModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(32), unique=False)
-    quote = db.Column(db.String(255), unique=False)
-
-    def __init__(self, author, quote):
-        self.author = author
-        self.quote = quote
-
-    def __repr__(self):
-        return f"Quote: {self.author}/ {self.quote[:10]}..."
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "author": self.author,
-            "text": self.quote,
-        }
-
 
 @app.route("/quotes")
 def quotes_list():
-    quotes = QuoteModel.query.all()
-    new_quotes = []
-    for quote in quotes:
-        new_quotes.append(quote.to_dict())
-    return jsonify(new_quotes)
+    query = "SELECT * FROM quotes"
+    connection = create_connection(BASE_DIR / 'db.sqlite')
+    results = execute_read_query(connection, query)
+    keys = ("id", "author", "text")
+    quotes = []
+    for data in results:
+        quote = dict(zip(keys, data))
+        quotes.append(quote)
+    return jsonify(quotes)
 
 
 @app.route("/quotes/<int:id>")
 def get_quote(id):
-    quote = QuoteModel.query.get(id)
-    if quote is None:
+    query = f"SELECT * FROM quotes WHERE id={id}"
+    connection = create_connection(BASE_DIR / 'db.sqlite')
+    data = execute_read_query(connection, query, only_one=True)
+    if data is None:
         abort(404, description=f"Quote with id = {id} is not found.")
-    return jsonify(quote.to_dict())
+    keys = ("id", "author", "text")
+    quote = dict(zip(keys, data))
+    return jsonify(quote)
 
 
 @app.route("/count_quotes")
@@ -85,11 +65,13 @@ def count_quotes():
 @app.route("/quotes", methods=["POST"])
 def create_quote():
     new_quote = request.json
-    # quote = QuoteModel(new_quote["author"], new_quote["quote"])
-    quote = QuoteModel(**new_quote)
-    db.session.add(quote)
-    db.session.commit()
-    return jsonify(quote.to_dict()), 201
+    try:
+        query = f"INSERT INTO quotes (author,text) VALUES ('{new_quote['author']}', '{new_quote['text']}')"
+    except KeyError:
+        abort(400, "field author and text required")
+    connection = create_connection(BASE_DIR / 'db.sqlite')
+    execute_query(connection, query)
+    return {}, 201  # Created
 
 
 @app.route("/quotes/<int:id>", methods=["PUT"])
